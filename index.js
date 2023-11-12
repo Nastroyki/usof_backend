fd7bf92905e1ec46d324b25a52dc0a815a878ab0
Maksym Cherednychenko <116967129+Nastroyki@users.noreply.github.com>
2023-11-12 23:05:29 +0200

    prefinal2

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const auth = require('./middleware/auth');
 const authController = require('./controllers/authController');
 const userController = require('./controllers/userController');
 const postController = require('./controllers/postController');
+const commentController = require('./controllers/commentController');
+const commentAnswerController = require('./controllers/commentAnswerController');
+const tagController = require('./controllers/tagController');
 
 
 app.use(express.json());
@@ -15,6 +18,9 @@ app.use(express.json());
 app.use('/api/auth', authController);
 app.use('/api/users', userController);
 app.use('/api/posts', postController);
+app.use('/api/comments', commentController);
+app.use('/api/comment-answers', commentAnswerController);
+app.use('/api/tags', tagController);
 
 
 app.listen(PORT, () => {
